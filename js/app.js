1ba5ec416b81e4352fefb68e104d0f2bb69ae00e
CHAK <Marco@Marcos-MacBook.local>
2021-02-15 14:45:00 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	// $('body').hide()
+
+	var mySwiper = new Swiper('.swiper-container', {
+		// Optional parameters
+		loop: true,
+
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+		autoplay: {
+		  delay: 5000,
+		  disableOnInteraction: false,
+		},
+	  });
+
 })
