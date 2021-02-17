1d8bf59f5c51382fb39410c0c42759720c8f051c
CHAK <Marco@Marcos-MacBook.local>
2021-02-17 20:39:19 +0200

    edited code, edited viewport,added menu-button

--- app/js/app.js
+++ app/js/app.js
@@ -7,13 +7,35 @@ $(document).ready(function () {
 
 		// Navigation arrows
 		navigation: {
-		  nextEl: '.swiper-button-next',
-		  prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
 		},
 		autoplay: {
-		  delay: 5000,
-		  disableOnInteraction: false,
+			delay: 5000,
+			disableOnInteraction: false,
 		},
-	  });
+	});
 
-})
+	//Login modal
+
+	$(".js-login").on("click", function (e) {
+		e.preventDefault();
+		$(".modal--login").fadeIn(400);
+	});
+
+	$(".modal__close").on("click", function (e) {
+		e.preventDefault();
+		$(".modal--login").fadeOut(400);
+	})
+
+	$(document).on("mouseup", function(event) {
+		const loginModal = $('.modal__window');
+
+	//не модальное окно и не содержащие элементы!!
+
+		if(!loginModal.is(event.target)
+			&& loginModal.has(event.target).length === 0) {
+				$(".modal").fadeOut(400);
+		}
+	})
+});
